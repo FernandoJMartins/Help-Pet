aedc2a4fd16868c015c693542a09b42239489b7a
Fernando Julio <julio8martins@gmail.com>
2025-07-22 23:18:10 -0300

    bottom menu

--- app/src/main/java/com/example/helppet/MainActivity.kt
+++ app/src/main/java/com/example/helppet/MainActivity.kt
@@ -14,7 +14,10 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
@@ -31,13 +34,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.helppet.ui.theme.HelpPetTheme
-
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -45,13 +51,38 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HelpPetTheme {
-            Home()
+            MainScreen()
         }
     }
 }}
 
+
+
 @Composable
-fun Home() {
+fun MainScreen(){
+    var selectedItem by remember { mutableStateOf<BottomNavItem>(BottomNavItem.Home) }
+
+    Scaffold(
+        bottomBar = {
+            BottomNavigationBar(
+                selectedItem = selectedItem,
+                onItemSelected = { selectedItem = it }
+            )
+        }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            when (selectedItem) {
+                BottomNavItem.Home -> HomeScreen()
+                BottomNavItem.NewReport -> NewReportScreen()
+                BottomNavItem.ListReports -> ListReportsScreen()
+                BottomNavItem.Profile -> ProfileScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeScreen() {
     var nome by remember { mutableStateOf("") }
     var tipo by remember { mutableStateOf("Cachorro") }
     var descricao by remember { mutableStateOf("") }
@@ -158,10 +189,72 @@ fun Home() {
     }
 }
 
+
+@Composable
+fun NewReportScreen() {
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("Nova Ocorrência")
+    }
+}
+
+
+
+@Composable
+fun ListReportsScreen(){
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("OCORRENIAS ATUTIAIITIAIIS")
+    }
+}
+
+
+@Composable
+fun ProfileScreen(){
+    Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+        Text("PROFILE SCRNEENENNE")
+    }
+}
+
+
+
+
+
+
+sealed class BottomNavItem(val label: String, val icon: ImageVector) {
+    object Home : BottomNavItem("Home", Icons.Filled.Home)
+    object NewReport : BottomNavItem("Nova", Icons.Filled.AddCircle)
+    object ListReports : BottomNavItem("Ocorrências", Icons.Filled.Search)
+    object Profile : BottomNavItem("Perfil", Icons.Filled.Person)
+}
+
+
+@Composable
+fun BottomNavigationBar(
+    selectedItem: BottomNavItem,
+    onItemSelected: (BottomNavItem) -> Unit
+) {
+    NavigationBar {
+        listOf(
+            BottomNavItem.Home,
+            BottomNavItem.NewReport,
+            BottomNavItem.ListReports,
+            BottomNavItem.Profile
+        ).forEach { item ->
+            NavigationBarItem(
+                icon = { Icon(item.icon, contentDescription = item.label) },
+                label = { Text(item.label) },
+                selected = item == selectedItem,
+                onClick = { onItemSelected(item) }
+            )
+        }
+    }
+}
+
+
+
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun Preview() {
     HelpPetTheme {
-        Home()
+        MainScreen()
     }
 }
